ecbbdb60b2809b296578a4408a66ae1396fe7084
Skywonda <princesam3600@gmail.com>
2022-11-08 00:23:05 +0100

    adjustment

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
     grid-gap: 30px;
     align-item: center;
     width: 100%;
